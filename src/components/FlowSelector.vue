251cc2a84df4b30dead86775082552eb6f912467
datomo <dave.lengw@gmail.com>
2020-12-26 11:33:43 +0100

    added barebones flowselector

---
+++ src/components/FlowSelector.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="flow-selector">
+    <div class="popup">
+      <div class="select">
+        <select name="cars" id="cars">
+          <option v-for="block in blocks" :value="block.id" :key="block.id">{{block.name}}</option>
+        </select>
+      </div>
+      <div class="blocks">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import {useGetters} from "@/helpers/store";
+
+export default {
+  name: "FlowSelector",
+  setup() {
+    const {blocks} = useGetters(["blocks"]);
+
+    return {
+      blocks
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.flow-selector {
+  position: absolute;
+  z-index: 1;
+  height: 100%;
+  width: 100%;
+  background-color: black;
+  top: 0;
+  left: 0;
+  display: grid;
+
+  .popup {
+    background-color: gray;
+    display: inline-block;
+    padding: 1rem;
+    align-self: center;
+    justify-self: center;
+  }
+}
+</style>
--- src/views/Flow.vue
+++ src/views/Flow.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="home">
+  <div class="flow">
     <h1>Flow</h1>
-    <div class="flow">
+    <div class="flow-blocks">
       <Block v-for="block in root" :id="block.id" :key="block.id"/>
     </div>
-    <Adder/>
+    <Adder @click="openAdder"/>
+    <FlowSelector v-if="popupVisible"/>
   </div>
 </template>
 
@@ -12,18 +13,26 @@
 import {useGetters} from "@/helpers/store";
 import Block from "@/components/Description";
 import Adder from "@/components/Adder";
+import FlowSelector from "@/components/FlowSelector";
+import {ref} from "vue";
 
 export default {
   name: 'Flow',
   components: {
     Block,
-    Adder
+    Adder,
+    FlowSelector
   },
   setup() {
     const {root} = useGetters(["root"]);
+    const popupVisible = ref(false)
+
+    const openAdder = () => {
+      popupVisible.value = !popupVisible.value;
+    }
 
     return {
-      root
+      root, openAdder, popupVisible
     }
   },
 }
@@ -34,4 +43,10 @@ export default {
 h1, h2, h3 {
   font-weight: normal;
 }
+
+.flow {
+  position: relative;
+  height: 100%;
+}
+
 </style>
